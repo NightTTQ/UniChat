7ba420cb4f3ef4a34276bfe9b8b53e0bec248681
TTQ <1932170807@qq.com>
2022-12-07 18:57:22 +0800

    全局主题控制

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-config-provider :theme="useDarkTheme === true ? darkTheme : null">
+    <n-button @click="useDarkTheme = !useDarkTheme"> 切换主题 </n-button>
+    <router-view />
+    <n-global-style />
+  </n-config-provider>
 </template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { darkTheme, useOsTheme } from "naive-ui";
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+const useDarkTheme = ref(useOsTheme().value === "dark" ? true : false);
+</script>
+<style scoped></style>
