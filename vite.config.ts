e46a8670702cf0e44355d28d004da52345d337c3
TTQ <1932170807@qq.com>
2023-01-06 21:53:39 +0800

    更新项目配置 修改开发环境后端端口

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:7001",
+        target: "http://localhost:3000",
         changeOrigin: true,
         ws: true,
         secure: false,
