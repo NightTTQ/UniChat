ee107c7e97e1b89f30852b60b31cf1ef5b5417ef
TTQ <1932170807@qq.com>
2023-01-07 12:01:34 +0800

    引入axios并实现实例

---
+++ src/services/axios/request.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// axios实例
+const instance = axios.create({
+  baseURL: "api",
+  timeout: 300000,
+  headers: {},
+});
+
+// 请求拦截器
+instance.interceptors.request.use(
+  (req) => {
+    return req;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// 响应拦截器
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
